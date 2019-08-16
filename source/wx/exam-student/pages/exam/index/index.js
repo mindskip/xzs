15c912243b4193dc97f1a3904311d3fe3c88665e
alvis <yuhaoo@qq.com>
2019-08-16 11:41:44 +0800

    update

--- source/wx/exam-student/pages/exam/index/index.js
+++ source/wx/exam-student/pages/exam/index/index.js
@@ -1,71 +1,80 @@
 // pages/exam/index/index.js
+let app = getApp()
 Page({
 
   /**
    * Page initial data
    */
   data: {
-    current: 'tab1',
+    paperType: '1',
+    spinShow: false,
+    queryParam: {
+      paperType: '1',
+      pageIndex: 1,
+      pageSize: 10
+    },
+    tableData: [],
+    total: 1
   },
 
   /**
    * Lifecycle function--Called when page load
    */
-  onLoad: function (options) {
-
+  onLoad: function(options) {
+    this.search()
   },
-
-  handleChange({ detail }) {
+  tabChange({
+    detail
+  }) {
     this.setData({
-      current: detail.key
+      paperType: detail.key,
+      queryParam: {
+        paperType: detail.key,
+        pageIndex: 1,
+        pageSize: 10
+      }
     });
+    this.search()
   },
-  /**
-   * Lifecycle function--Called when page is initially rendered
-   */
-  onReady: function () {
-
+  pageChange({
+    detail
+  }) {
+    const type = detail.type;
+    if (type === 'next') {
+      this.setData({
+        queryParam: {
+          pageIndex: this.data.queryParam.pageIndex + 1,
+        }
+      });
+    } else if (type === 'prev') {
+      this.setData({
+        queryParam: {
+          pageIndex: this.data.queryParam.pageIndex - 1,
+        }
+      });
+    }
   },
-
-  /**
-   * Lifecycle function--Called when page show
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * Lifecycle function--Called when page hide
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * Lifecycle function--Called when page unload
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * Page event handler function--Called when user drop down
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * Called when page reach bottom
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * Called when user click on the top right corner to share
-   */
-  onShareAppMessage: function () {
-
+  search: function() {
+    let _this = this
+    _this.setData({
+      spinShow: false
+    });
+    app.formPost('/api/wx/student/exam/pageList', this.data.queryParam, function (data) {
+      if (data.code === 1) {
+        const re = data.response
+        _this.setData({
+          queryParam: {
+            pageIndex: re.pageNum,
+            pageSize: 10
+          },
+          tableData: re.list,
+          total: re.pageNum
+        });
+      }
+    }, function() {
+      _this.setData({
+        spinShow: false
+      });
+    })
   }
 })
