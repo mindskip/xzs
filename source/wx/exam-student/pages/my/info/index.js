ff0f96e8ca9b61343e09fd954fe51ec619c5eb93
alvis <yuhaoo@qq.com>
2019-08-28 14:41:00 +0800

    样式调整

--- source/wx/exam-student/pages/my/info/index.js
+++ source/wx/exam-student/pages/my/info/index.js
@@ -10,12 +10,14 @@ Page({
     levelVal: null,
     position: 'right',
   },
-  handleSexChange({ detail = {} }) {
+  handleSexChange({
+    detail = {}
+  }) {
     this.setData({
       sexVal: detail.value
     });
-    for(let i = 0;i < this.data.sexItems.length; i++){
-      if(this.data.sexItems[i].value == detail.value){
+    for (let i = 0; i < this.data.sexItems.length; i++) {
+      if (this.data.sexItems[i].value == detail.value) {
         this.setData({
           sexId: this.data.sexItems[i].key
         });
@@ -23,7 +25,9 @@ Page({
     }
   },
   bindDateChange(e) {
-    let { value } = e.detail;
+    let {
+      value
+    } = e.detail;
     this.setData({
       birthDay: value
     })
@@ -59,48 +63,45 @@ Page({
   },
   formSubmit: function(e) {
     let _this = this
-    _this.setData({
-      spinShow: true
-    });
+    wx.showLoading({
+      title: '提交中',
+      mask: true
+    })
     e.detail.value.sex = this.data.sexId;
     e.detail.value.userLevel = this.data.levelVal;
     app.formPost('/api/wx/student/user/update', e.detail.value)
-        .then(res => {
-          _this.setData({
-            spinShow: false
-          });
-          if (res.code == 1) {
-            this.loadUserInfo();
-            wx.switchTab({
-              url: '/pages/my/index/index'
-            })
-          } else {
-            app.message(res.message, 'error')
-          }
-        }).catch(e => {
-      _this.setData({
-        spinShow: false
-      });
-      app.message(e, 'error')
-    })
+      .then(res => {
+        if (res.code == 1) {
+          this.loadUserInfo();
+          wx.switchTab({
+            url: '/pages/my/index/index'
+          })
+        } else {
+          app.message(res.message, 'error')
+        }
+        wx.hideLoading()
+      }).catch(e => {
+        app.message(e, 'error')
+        wx.hideLoading()
+      })
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
     let userInfo = app.globalData.userInfo;
     let _this = this;
-    for(let i = 0;i < this.data.sexItems.length; i++){
-      if(this.data.sexItems[i].key == userInfo.sex){
+    for (let i = 0; i < this.data.sexItems.length; i++) {
+      if (this.data.sexItems[i].key == userInfo.sex) {
         _this.setData({
           sexVal: this.data.sexItems[i].value,
           sexId: userInfo.sex
         });
       }
     }
-    for(let i = 0;i < this.data.levelItems.length; i++){
-      if(this.data.levelItems[i].key == userInfo.userLevel){
+    for (let i = 0; i < this.data.levelItems.length; i++) {
+      if (this.data.levelItems[i].key == userInfo.userLevel) {
         _this.setData({
           levelVal: this.data.levelItems[i].key,
           levelIndex: i
@@ -114,54 +115,5 @@ Page({
       phone: userInfo.phone,
       userLevel: userInfo.userLevel,
     });
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
   }
 })
