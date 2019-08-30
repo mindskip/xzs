e5d2bc1c3db3f8a469bc0e44a8de058ef7dfbcf8
alvis <yuhaoo@qq.com>
2019-08-30 11:29:39 +0800

    小程序 上下拉加载

--- source/wx/exam-student/pages/record/index.js
+++ source/wx/exam-student/pages/record/index.js
@@ -1,57 +1,64 @@
 // pages/exam/index/index.js
 let app = getApp()
 Page({
-
-  /**
-   * Page initial data
-   */
   data: {
     spinShow: false,
+    loadMoreLoad: false,
+    loadMoreTip: '正在加载',
     queryParam: {
       pageIndex: 1,
-      pageSize: 8
+      pageSize: 10
     },
     tableData: [],
     total: 1
   },
-
-  /**
-   * Lifecycle function--Called when page load
-   */
   onLoad: function(options) {
-    this.search()
+    this.setData({
+      spinShow: true
+    });
+    this.search(true)
   },
-  pageChange({
-    detail
-  }) {
-    const type = detail.type;
-    if (type === 'next') {
+  onPullDownRefresh() {
+    this.setData({
+      spinShow: true
+    });
+    if (!this.loading) {
+      this.setData({
+        ['queryParam.pageIndex']: 1
+      });
+      this.search(true)
+    }
+  },
+  onReachBottom() {
+    if (!this.loading && this.data.queryParam.pageIndex < this.data.total) {
+      this.setData({
+        loadMoreLoad: true,
+        loadMoreTip: '正在加载'
+      });
       this.setData({
         ['queryParam.pageIndex']: this.data.queryParam.pageIndex + 1
       });
-    } else if (type === 'prev') {
+      this.search(false)
+    } else {
       this.setData({
-        ['queryParam.pageIndex']: this.data.queryParam.pageIndex - 1
+        loadMoreLoad: false,
+        loadMoreTip: '暂无数据'
       });
     }
-    this.search()
   },
-  search: function() {
+  search: function(override) {
     let _this = this
-    _this.setData({
-      spinShow: true
-    });
     app.formPost('/api/wx/student/exampaper/answer/pageList', this.data.queryParam)
       .then(res => {
         _this.setData({
           spinShow: false
         });
+        wx.stopPullDownRefresh()
         if (res.code === 1) {
           const re = res.response
           _this.setData({
             ['queryParam.pageIndex']: re.pageNum,
-            ['queryParam.pageSize']: 8,
-            tableData: re.list,
+            tableData: override ? re.list : this.data.tableData.concat(re.list),
             total: re.pages
           });
         }
