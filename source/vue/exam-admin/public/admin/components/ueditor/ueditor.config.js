1f833caa182a8bf22e1a89800b33a80912c71330
alvis <yuhaoo@qq.com>
2019-09-10 12:50:39 +0800

    update image config

--- source/vue/exam-admin/public/admin/components/ueditor/ueditor.config.js
+++ source/vue/exam-admin/public/admin/components/ueditor/ueditor.config.js
@@ -21,7 +21,7 @@
   window.UEDITOR_HOME_URL = '/admin/components/ueditor/'
   var URL = window.UEDITOR_HOME_URL || getUEBasePath()
 
-  var serverUrl = 'localhost:80'
+  var serverUrl = 'www.alvisu:8001'
 
   /**
    * 配置项主体。注意，此处所有涉及到路径的配置别遗漏URL变量。
@@ -32,7 +32,7 @@
     UEDITOR_HOME_URL: URL,
 
     // 服务器统一请求接口路径'http://' + serverUrl +"/core/upload/ueditor"
-    serverUrl: 'http://' + serverUrl + '/api/admin/upload/configAndUpload',
+    serverUrl: 'https://' + serverUrl + '/api/admin/upload/configAndUpload',
 
     // 工具栏上的所有的功能按钮和下拉框，可以在new编辑器的实例时选择自己需要的重新定义。,'custombtn'插入横线,'complexformulas'复杂公式,
     toolbars: [[
