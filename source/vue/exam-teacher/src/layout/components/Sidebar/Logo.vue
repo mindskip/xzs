88f840ee83668b91adac94651bb2b3eb500c66d4
alvis <yuhaoo@qq.com>
2019-09-12 19:23:56 +0800

    update logo

--- source/vue/exam-teacher/src/layout/components/Sidebar/Logo.vue
+++ source/vue/exam-teacher/src/layout/components/Sidebar/Logo.vue
@@ -54,8 +54,7 @@ export default {
     width: 100%;
 
     & .sidebar-logo {
-      width: 32px;
-      height: 32px;
+      width: 45px;
       vertical-align: middle;
       margin-right: 12px;
     }
