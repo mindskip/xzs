57c275e24226ba3a9216bb50a706e4c927cca791
wushaofeng <846162692@qq.com>
2020-12-29 17:17:23 +0800

    完成试卷生成的方法 test2--By Ternece

--- source/xzs/src/main/java/com/mindskip/xzs/service/impl/ExaminationPaperProductionServiceImpl.java
+++ source/xzs/src/main/java/com/mindskip/xzs/service/impl/ExaminationPaperProductionServiceImpl.java
@@ -1,6 +1,5 @@
 package com.mindskip.xzs.service.impl;
 
-import com.github.pagehelper.PageHelper;
 import com.mindskip.xzs.domain.Question;
 import com.mindskip.xzs.repository.BaseMapper;
 import com.mindskip.xzs.repository.QuestionMapper;
@@ -27,7 +26,6 @@ public class ExaminationPaperProductionServiceImpl extends BaseServiceImpl<Quest
     @Override
     public List<Question> generation(ExaminationProductionVM examinationProductionVM) {
 
-        return PageHelper.startPage(examinationProductionVM.getPageIndex(),examinationProductionVM.getPageSize()).doSelectPage(()->
-                questionMapper.selectQuestionsByQuestionsNumber(examinationProductionVM));
+        return questionMapper.selectQuestionsByQuestionsNumber(examinationProductionVM);
     }
 }
