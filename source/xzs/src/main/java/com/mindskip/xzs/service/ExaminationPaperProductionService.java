07c790803b45a04164b5ee43ecd4c9c32b09d7d2
wushaofeng <846162692@qq.com>
2020-12-29 16:42:57 +0800

    完成试卷生成的方法--By Ternece

---
+++ source/xzs/src/main/java/com/mindskip/xzs/service/ExaminationPaperProductionService.java
@@ -0,0 +1,12 @@
+package com.mindskip.xzs.service;
+
+import com.mindskip.xzs.domain.Question;
+import com.mindskip.xzs.viewmodel.student.exam.ExaminationProductionVM;
+
+import java.util.List;
+
+public interface ExaminationPaperProductionService extends BaseService<Question>{
+
+
+    List<Question> generation(ExaminationProductionVM examinationProductionVM);
+}
