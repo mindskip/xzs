880b3b702d39cae8187f1cab198c4f89133c9bd1
alvis <alvis_u@qq.com>
2021-01-04 15:47:08 +0800

    移除lombok

--- source/xzs/src/main/java/com/mindskip/xzs/controller/admin/DashboardController.java
+++ source/xzs/src/main/java/com/mindskip/xzs/controller/admin/DashboardController.java
@@ -5,8 +5,6 @@ import com.mindskip.xzs.base.RestResponse;
 import com.mindskip.xzs.service.*;
 import com.mindskip.xzs.utility.DateTimeUtil;
 import com.mindskip.xzs.viewmodel.admin.dashboard.IndexVM;
-import com.mindskip.xzs.service.*;
-import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,15 +13,21 @@ import java.util.List;
 
 @RestController("AdminDashboardController")
 @RequestMapping(value = "/api/admin/dashboard")
-@AllArgsConstructor
 public class DashboardController extends BaseApiController {
-
 
     private final ExamPaperService examPaperService;
     private final QuestionService questionService;
     private final ExamPaperAnswerService examPaperAnswerService;
     private final ExamPaperQuestionCustomerAnswerService examPaperQuestionCustomerAnswerService;
     private final UserEventLogService userEventLogService;
+
+    public DashboardController(ExamPaperService examPaperService, QuestionService questionService, ExamPaperAnswerService examPaperAnswerService, ExamPaperQuestionCustomerAnswerService examPaperQuestionCustomerAnswerService, UserEventLogService userEventLogService) {
+        this.examPaperService = examPaperService;
+        this.questionService = questionService;
+        this.examPaperAnswerService = examPaperAnswerService;
+        this.examPaperQuestionCustomerAnswerService = examPaperQuestionCustomerAnswerService;
+        this.userEventLogService = userEventLogService;
+    }
 
     @RequestMapping(value = "/index", method = RequestMethod.POST)
     public RestResponse<IndexVM> Index() {
