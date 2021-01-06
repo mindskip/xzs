716fe7cb30eee7ded825bb3cdece54d7b3963c25
alvis <alvis_u@qq.com>
2021-01-06 10:39:11 +0800

    优化

--- source/xzs/src/main/java/com/mindskip/xzs/controller/student/UploadController.java
+++ source/xzs/src/main/java/com/mindskip/xzs/controller/student/UploadController.java
@@ -6,6 +6,7 @@ import com.mindskip.xzs.base.RestResponse;
 import com.mindskip.xzs.service.FileUpload;
 import com.mindskip.xzs.service.UserService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,6 +26,7 @@ public class UploadController extends BaseApiController {
     private final FileUpload fileUpload;
     private final UserService userService;
 
+    @Autowired
     public UploadController(FileUpload fileUpload, UserService userService) {
         this.fileUpload = fileUpload;
         this.userService = userService;
