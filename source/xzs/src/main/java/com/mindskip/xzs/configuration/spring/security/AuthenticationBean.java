61adcc0b3232281bd050d28565fb66e4d0653d22
alvis <alvis_u@qq.com>
2021-01-11 09:18:12 +0800

    优化

--- source/xzs/src/main/java/com/mindskip/xzs/configuration/spring/security/AuthenticationBean.java
+++ source/xzs/src/main/java/com/mindskip/xzs/configuration/spring/security/AuthenticationBean.java
@@ -1,14 +1,37 @@
 package com.mindskip.xzs.configuration.spring.security;
 
-import lombok.Data;
 
 /**
  * @author 武汉思维跳跃科技有限公司
  */
 
-@Data
 public class AuthenticationBean {
     private String userName;
     private String password;
     private boolean remember;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isRemember() {
+        return remember;
+    }
+
+    public void setRemember(boolean remember) {
+        this.remember = remember;
+    }
+
 }
