12e17076e0518a8931f5a0a1ecbb334184274681
alvis <alvis_u@qq.com>
2021-01-15 09:13:33 +0800

    优化

--- source/xzs/src/main/java/com/mindskip/xzs/domain/exam/ExamPaperQuestionItemObject.java
+++ source/xzs/src/main/java/com/mindskip/xzs/domain/exam/ExamPaperQuestionItemObject.java
@@ -1,8 +1,5 @@
 package com.mindskip.xzs.domain.exam;
 
-import lombok.Data;
-
-@Data
 public class ExamPaperQuestionItemObject {
     private Integer id;
     private Integer itemOrder;
--- source/xzs/src/main/java/com/mindskip/xzs/domain/question/QuestionItemObject.java
+++ source/xzs/src/main/java/com/mindskip/xzs/domain/question/QuestionItemObject.java
@@ -1,8 +1,6 @@
 package com.mindskip.xzs.domain.question;
 
-import lombok.Data;
 
-@Data
 public class QuestionItemObject {
 
     private String prefix;
@@ -10,4 +8,28 @@ public class QuestionItemObject {
     private String content;
 
     private Integer score;
+
+    public String getPrefix() {
+        return prefix;
+    }
+
+    public void setPrefix(String prefix) {
+        this.prefix = prefix;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Integer getScore() {
+        return score;
+    }
+
+    public void setScore(Integer score) {
+        this.score = score;
+    }
 }
