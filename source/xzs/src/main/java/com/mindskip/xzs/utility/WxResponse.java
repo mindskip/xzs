492aa3e60a2dae3ad3b11b984294c3a852f60931
alvis <alvis_u@qq.com>
2021-01-18 10:05:53 +0800

    优化

--- source/xzs/src/main/java/com/mindskip/xzs/utility/WxResponse.java
+++ source/xzs/src/main/java/com/mindskip/xzs/utility/WxResponse.java
@@ -1,12 +1,30 @@
 package com.mindskip.xzs.utility;
 
-import lombok.Data;
 
 import java.io.Serializable;
 
-@Data
 public class WxResponse implements Serializable {
     private static final long serialVersionUID = -8496869159673561976L;
     private String session_key;
     private String openid;
+
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+
+    public String getSession_key() {
+        return session_key;
+    }
+
+    public void setSession_key(String session_key) {
+        this.session_key = session_key;
+    }
+
+    public String getOpenid() {
+        return openid;
+    }
+
+    public void setOpenid(String openid) {
+        this.openid = openid;
+    }
 }
