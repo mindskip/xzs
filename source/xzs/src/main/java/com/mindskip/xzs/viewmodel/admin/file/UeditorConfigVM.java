05b5e6ebdd0673532750d4ca45048bccc9d1b6f9
alvis <alvis_u@qq.com>
2021-01-20 08:56:17 +0800

    优化

--- source/xzs/src/main/java/com/mindskip/xzs/viewmodel/admin/file/UeditorConfigVM.java
+++ source/xzs/src/main/java/com/mindskip/xzs/viewmodel/admin/file/UeditorConfigVM.java
@@ -1,10 +1,10 @@
 package com.mindskip.xzs.viewmodel.admin.file;
 
-import lombok.Data;
+
 
 import java.util.List;
 
-@Data
+
 public class UeditorConfigVM {
     private String imageActionName;
     private  String imageFieldName;
@@ -15,4 +15,76 @@ public class UeditorConfigVM {
     private String imageInsertAlign;
     private String imageUrlPrefix;
     private String imagePathFormat;
+
+    public String getImageActionName() {
+        return imageActionName;
+    }
+
+    public void setImageActionName(String imageActionName) {
+        this.imageActionName = imageActionName;
+    }
+
+    public String getImageFieldName() {
+        return imageFieldName;
+    }
+
+    public void setImageFieldName(String imageFieldName) {
+        this.imageFieldName = imageFieldName;
+    }
+
+    public Long getImageMaxSize() {
+        return imageMaxSize;
+    }
+
+    public void setImageMaxSize(Long imageMaxSize) {
+        this.imageMaxSize = imageMaxSize;
+    }
+
+    public List<String> getImageAllowFiles() {
+        return imageAllowFiles;
+    }
+
+    public void setImageAllowFiles(List<String> imageAllowFiles) {
+        this.imageAllowFiles = imageAllowFiles;
+    }
+
+    public boolean isImageCompressEnable() {
+        return imageCompressEnable;
+    }
+
+    public void setImageCompressEnable(boolean imageCompressEnable) {
+        this.imageCompressEnable = imageCompressEnable;
+    }
+
+    public Integer getImageCompressBorder() {
+        return imageCompressBorder;
+    }
+
+    public void setImageCompressBorder(Integer imageCompressBorder) {
+        this.imageCompressBorder = imageCompressBorder;
+    }
+
+    public String getImageInsertAlign() {
+        return imageInsertAlign;
+    }
+
+    public void setImageInsertAlign(String imageInsertAlign) {
+        this.imageInsertAlign = imageInsertAlign;
+    }
+
+    public String getImageUrlPrefix() {
+        return imageUrlPrefix;
+    }
+
+    public void setImageUrlPrefix(String imageUrlPrefix) {
+        this.imageUrlPrefix = imageUrlPrefix;
+    }
+
+    public String getImagePathFormat() {
+        return imagePathFormat;
+    }
+
+    public void setImagePathFormat(String imagePathFormat) {
+        this.imagePathFormat = imagePathFormat;
+    }
 }
--- source/xzs/src/main/java/com/mindskip/xzs/viewmodel/admin/file/UploadResultVM.java
+++ source/xzs/src/main/java/com/mindskip/xzs/viewmodel/admin/file/UploadResultVM.java
@@ -1,8 +1,8 @@
 package com.mindskip.xzs.viewmodel.admin.file;
 
-import lombok.Data;
 
-@Data
+
+
 public class UploadResultVM {
     private String original;
     private String name;
@@ -10,4 +10,52 @@ public class UploadResultVM {
     private Long size;
     private String type;
     private String state;
+
+    public String getOriginal() {
+        return original;
+    }
+
+    public void setOriginal(String original) {
+        this.original = original;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public Long getSize() {
+        return size;
+    }
+
+    public void setSize(Long size) {
+        this.size = size;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
 }
