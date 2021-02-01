a7934c85478a2b625db90ccbb3b1c453f07e8b5f
alvis <alvis_u@qq.com>
2021-02-01 12:11:24 +0800

    成绩管理

--- source/xzs/src/main/java/com/mindskip/xzs/repository/ExamPaperAnswerMapper.java
+++ source/xzs/src/main/java/com/mindskip/xzs/repository/ExamPaperAnswerMapper.java
@@ -31,4 +31,5 @@ public interface ExamPaperAnswerMapper extends BaseMapper<ExamPaperAnswer> {
 
     ExamPaperAnswer getByPidUid(@Param("pid") Integer paperId, @Param("uid") Integer uid);
 
+    List<ExamPaperAnswer> adminPage(com.mindskip.xzs.viewmodel.admin.paper.ExamPaperAnswerPageRequestVM requestVM);
 }
