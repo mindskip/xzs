5f514b2f1aedde2369445adb621bd365a7637374
alvis <alvis_u@qq.com>
2021-05-10 08:58:32 +0800

    代码注释

--- source/xzs/src/main/java/com/mindskip/xzs/domain/User.java
+++ source/xzs/src/main/java/com/mindskip/xzs/domain/User.java
@@ -11,26 +11,47 @@ public class User implements Serializable {
 
     private String userUuid;
 
+    /**
+     * 用户名
+     */
     private String userName;
 
     private String password;
 
+    /**
+     * 真实姓名
+     */
     private String realName;
 
     private Integer age;
 
+    /**
+     * 1.男 2女
+     */
     private Integer sex;
 
     private Date birthDay;
 
+    /**
+     * 学生年级(1-12)
+     */
     private Integer userLevel;
 
     private String phone;
 
+    /**
+     * 1.学生  3.管理员
+     */
     private Integer role;
 
+    /**
+     * 1.启用 2禁用
+     */
     private Integer status;
 
+    /**
+     * 头像地址
+     */
     private String imagePath;
 
     private Date createTime;
@@ -39,8 +60,14 @@ public class User implements Serializable {
 
     private Date lastActiveTime;
 
+    /**
+     * 是否删除
+     */
     private Boolean deleted;
 
+    /**
+     * 微信openId
+     */
     private String wxOpenId;
 
     public Integer getId() {
