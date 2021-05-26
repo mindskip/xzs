1ff61883ebea4b3bc86d74926b044a37d3f8e7b1
alvis <alvis_u@qq.com>
2021-05-26 14:41:36 +0800

    代码注释

--- source/xzs/src/main/java/com/mindskip/xzs/base/BasePage.java
+++ source/xzs/src/main/java/com/mindskip/xzs/base/BasePage.java
@@ -1,28 +1,49 @@
 package com.mindskip.xzs.base;
 
-
 /**
- * @author 武汉思维跳跃科技有限公司
+ * @version 3.3.0
+ * @description: The type Base page.
+ * Copyright (C), 2020-2021, 武汉思维跳跃科技有限公司
+ * @date 2021/5/25 10:45
  */
-
 public class BasePage {
 
     private Integer pageIndex;
 
     private Integer pageSize;
 
+    /**
+     * Gets page index.
+     *
+     * @return the page index
+     */
     public Integer getPageIndex() {
         return pageIndex;
     }
 
+    /**
+     * Sets page index.
+     *
+     * @param pageIndex the page index
+     */
     public void setPageIndex(Integer pageIndex) {
         this.pageIndex = pageIndex;
     }
 
+    /**
+     * Gets page size.
+     *
+     * @return the page size
+     */
     public Integer getPageSize() {
         return pageSize;
     }
 
+    /**
+     * Sets page size.
+     *
+     * @param pageSize the page size
+     */
     public void setPageSize(Integer pageSize) {
         this.pageSize = pageSize;
     }
