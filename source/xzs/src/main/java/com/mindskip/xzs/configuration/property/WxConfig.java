fc328af623f8e4b8658014ac79717478bd366b9d
alvis <alvis_u@qq.com>
2021-09-29 10:04:39 +0800

    update description

--- source/xzs/src/main/java/com/mindskip/xzs/configuration/property/WxConfig.java
+++ source/xzs/src/main/java/com/mindskip/xzs/configuration/property/WxConfig.java
@@ -4,6 +4,12 @@ package com.mindskip.xzs.configuration.property;
 import java.time.Duration;
 import java.util.List;
 
+/**
+ * @version 3.3.0
+ * @description: The type Wx config.
+ * Copyright (C), 2019-2021, 武汉思维跳跃科技有限公司
+ * @date 2021 /5/26 10:45
+ */
 public class WxConfig {
 
 
@@ -12,34 +18,74 @@ public class WxConfig {
     private Duration tokenToLive;
     private List<String> securityIgnoreUrls;
 
+    /**
+     * Gets appid.
+     *
+     * @return the appid
+     */
     public String getAppid() {
         return appid;
     }
 
+    /**
+     * Sets appid.
+     *
+     * @param appid the appid
+     */
     public void setAppid(String appid) {
         this.appid = appid;
     }
 
+    /**
+     * Gets secret.
+     *
+     * @return the secret
+     */
     public String getSecret() {
         return secret;
     }
 
+    /**
+     * Sets secret.
+     *
+     * @param secret the secret
+     */
     public void setSecret(String secret) {
         this.secret = secret;
     }
 
+    /**
+     * Gets token to live.
+     *
+     * @return the token to live
+     */
     public Duration getTokenToLive() {
         return tokenToLive;
     }
 
+    /**
+     * Sets token to live.
+     *
+     * @param tokenToLive the token to live
+     */
     public void setTokenToLive(Duration tokenToLive) {
         this.tokenToLive = tokenToLive;
     }
 
+    /**
+     * Gets security ignore urls.
+     *
+     * @return the security ignore urls
+     */
     public List<String> getSecurityIgnoreUrls() {
         return securityIgnoreUrls;
     }
 
+    /**
+     * Sets security ignore urls.
+     *
+     * @param securityIgnoreUrls the security ignore urls
+     */
     public void setSecurityIgnoreUrls(List<String> securityIgnoreUrls) {
         this.securityIgnoreUrls = securityIgnoreUrls;
     }
