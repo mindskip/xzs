bec36bb2fbd113167fcd49c64251669db6084f86
alvis <alvis_u@qq.com>
2021-12-06 09:48:06 +0800

    修复部分机器接口不兼容问题

--- source/xzs/src/main/java/com/mindskip/xzs/repository/ExamPaperQuestionCustomerAnswerMapper.java
+++ source/xzs/src/main/java/com/mindskip/xzs/repository/ExamPaperQuestionCustomerAnswerMapper.java
@@ -12,17 +12,6 @@ import java.util.List;
 
 @Mapper
 public interface ExamPaperQuestionCustomerAnswerMapper extends BaseMapper<ExamPaperQuestionCustomerAnswer> {
-    int deleteByPrimaryKey(Integer id);
-
-    int insert(ExamPaperQuestionCustomerAnswer record);
-
-    int insertSelective(ExamPaperQuestionCustomerAnswer record);
-
-    ExamPaperQuestionCustomerAnswer selectByPrimaryKey(Integer id);
-
-    int updateByPrimaryKeySelective(ExamPaperQuestionCustomerAnswer record);
-
-    int updateByPrimaryKey(ExamPaperQuestionCustomerAnswer record);
 
     List<ExamPaperQuestionCustomerAnswer> selectListByPaperAnswerId(Integer id);
 
--- source/xzs/src/main/java/com/mindskip/xzs/repository/UserMapper.java
+++ source/xzs/src/main/java/com/mindskip/xzs/repository/UserMapper.java
@@ -16,20 +16,6 @@ import java.util.Map;
 @Mapper
 public interface UserMapper extends BaseMapper<User> {
 
-
-    int deleteByPrimaryKey(Integer id);
-
-    int insert(User record);
-
-    int insertSelective(User record);
-
-    User selectByPrimaryKey(Integer id);
-
-    int updateByPrimaryKeySelective(User record);
-
-    int updateByPrimaryKey(User record);
-
-
     /**
      * getAllUser
      *
