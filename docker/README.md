6ecaa8b826b6a80d540871fd727579c2b3aaaa23
思维跳跃科技 <mindskip@qq.com>
2023-05-12 11:54:13 +0800

    docker部署方案修改

---
+++ docker/README.md
@@ -0,0 +1,25 @@
+### 6.3 docker部署
+
+* 打开网站<https://gitee.com/mindskip/xzs-mysql>，找到docker目录，里面有已配置好的文件
+* 下载sql脚本，下载教程<https://www.mindskip.net:999>，然后解压sql压缩包，找到xzs-mysql.sql文件，编辑此文件，在文件开头加如下代码：
+
+```xzs-mysql
+CREATE DATABASE `xzs` CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
+USE xzs;
+```
+* sql文件改好后，将文件移动到 docker/sql 目录下
+* 将整个docker目录中的文件，复制到/usr/local/xzs下面
+* 进入到install目录，执行下面命令，安装docker-compose
+```docker-compose
+cd /usr/local/xzs/install
+mv docker-compose-linux-x86_64 /usr/local/bin/docker-compose
+chmod +x  /usr/local/bin/docker-compose
+docker-compose --version
+```
+* 执行下面命令，启动学之思网站，有问题可以看下/usr/local/xzs/log中的日志
+```docker-xzs
+cd /usr/local/xzs
+docker-compose up -d
+```
+* 学生端访问地址为：<http://ip:8000/student>
+* 管理员端访问地址为：<http://ip:8000/admin>
---
+++ docs/assets/develop.html.887ca395.js
@@ -0,0 +1,77 @@
+import{_ as l,r as d,o as a,c as r,a as e,b as n,d as s,e as t}from"./app.17b3736e.js";const c={},o=e("h1",{id:"_5-\u9879\u76EE\u5F00\u53D1",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#_5-\u9879\u76EE\u5F00\u53D1","aria-hidden":"true"},"#"),n(" 5. \u9879\u76EE\u5F00\u53D1")],-1),v=e("h3",{id:"_5-1-\u5F00\u53D1\u73AF\u5883",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#_5-1-\u5F00\u53D1\u73AF\u5883","aria-hidden":"true"},"#"),n(" 5.1 \u5F00\u53D1\u73AF\u5883")],-1),u=e("thead",null,[e("tr",null,[e("th",null,"\u73AF\u5883"),e("th",null,"\u7248\u672C"),e("th",null,"\u4E0B\u8F7D\u5730\u5740")])],-1),m=e("td",null,"NodeJs",-1),h=e("td",null,"16",-1),b={href:"https://nodejs.org/download/release/latest-v16.x/",target:"_blank",rel:"noopener noreferrer"},_=e("td",null,"Jdk",-1),p=e("td",null,"1.8",-1),x={href:"https://www.oracle.com/java/technologies/downloads/#java8-windows",target:"_blank",rel:"noopener noreferrer"},g=e("td",null,"Mysql",-1),w=e("td",null,"8.0",-1),f={href:"https://dev.mysql.com/downloads/windows/installer/8.0.html",target:"_blank",rel:"noopener noreferrer"},y=e("h3",{id:"_5-2-\u5F00\u53D1\u8BF4\u660E",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#_5-2-\u5F00\u53D1\u8BF4\u660E","aria-hidden":"true"},"#"),n(" 5.2 \u5F00\u53D1\u8BF4\u660E")],-1),k={href:"https://www.mindskip.net:999",target:"_blank",rel:"noopener noreferrer"},z=e("li",null,"\u4EE3\u7801\u4E0B\u8F7D postgresql \u7248\u672C\uFF0C\u914D\u5408\u76F8\u5E94\u7684\u6570\u636E\u5E93\u4F7F\u7528",-1),q=e("li",null,"\u5B89\u88C5postgresql \uFF0C\u5BFC\u5165xzx-postgresql.sql\u811A\u672C",-1),j=e("li",null,"\u5B66\u751F\u7AEF\u9ED8\u8BA4\u8D26\u53F7\uFF1Astudent / 123456",-1),I=e("li",null,"\u7BA1\u7406\u7AEF\u9ED8\u8BA4\u8D26\u53F7\uFF1Aadmin / 123456",-1),B=e("h3",{id:"_5-3-\u540E\u7AEF\u5F00\u53D1",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#_5-3-\u540E\u7AEF\u5F00\u53D1","aria-hidden":"true"},"#"),n(" 5.3 \u540E\u7AEF\u5F00\u53D1")],-1),N=e("li",null,"/uexam/source/xzs\u4E3A\u540E\u53F0\u4EE3\u7801\uFF0C\u5EFA\u8BAE\u4F7F\u7528IntelliJ IDEA\u6253\u5F00",-1),T=e("li",null,"\u6253\u5F00application-dev.yml\u6587\u4EF6\u4E2D\uFF0C\u914D\u7F6E\u597Dpostgesql\u7684\u670D\u52A1\u5730\u5740",-1),V=e("li",null,"\u53BB\u4E03\u725B\u4E91\u5B98\u7F51\u7533\u8BF7\u597D\u5BF9\u8C61\u5B58\u50A8\u8D26\u53F7\uFF0C\u4FEE\u6539application.yml\u4E2D\u7684qn\u76F8\u5173\u7684\u914D\u7F6E\uFF0C\u4E03\u725B\u4E91\u4E3B\u8981\u7528\u4E8E\u6587\u4EF6\u5B58\u50A8",-1),E=e("li",null,"\u542F\u52A8\u540E\u53F0\u7A0B\u5E8F,\u9ED8\u8BA4\u7AEF\u53E3\u4E3A8000",-1),J={href:"http://ip:8000/student",target:"_blank",rel:"noopener noreferrer"},S={href:"http://ip:8000/admin",target:"_blank",rel:"noopener noreferrer"},L=t(`<h3 id="_5-4-\u524D\u7AEF\u5F00\u53D1" tabindex="-1"><a class="header-anchor" href="#_5-4-\u524D\u7AEF\u5F00\u53D1" aria-hidden="true">#</a> 5.4 \u524D\u7AEF\u5F00\u53D1</h3><ul><li>\u524D\u7AEF\u4F7F\u7528webstorm\u6216\u8005vscode\uFF0C\u5206\u522B\u6253\u5F00\u6587\u4EF6\u5939\u6253\u5F00\u6E90\u4EE3\u7801\\source\\vue\\xzs-student\u548Csource\\vue\\xzs-admin</li><li>\u6267\u884C\u4E0B\u97622\u4E2A\u547D\u4EE4\uFF0C\u5B89\u88C5node_module\uFF1A</li></ul><div class="language-npm ext-npm line-numbers-mode"><pre class="language-npm"><code>npm config set sass_binary_site https://npm.taobao.org/mirrors/node-sass/
+npm install --registry https://registry.npm.taobao.org  
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div></div></div><ul><li>\u6267\u884C\u4E0B\u9762\u547D\u4EE4\uFF0C\u542F\u52A8\u524D\u7AEF\u4EE3\u7801</li></ul><div class="language-npm ext-npm line-numbers-mode"><pre class="language-npm"><code>npm run serve
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div></div></div><ul><li>\u6253\u5305\u547D\u540D</li></ul><div class="language-npm ext-npm line-numbers-mode"><pre class="language-npm"><code>npm run build
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div></div></div><h3 id="_5-5-\u5C0F\u7A0B\u5E8F\u5F00\u53D1" tabindex="-1"><a class="header-anchor" href="#_5-5-\u5C0F\u7A0B\u5E8F\u5F00\u53D1" aria-hidden="true">#</a> 5.5 \u5C0F\u7A0B\u5E8F\u5F00\u53D1</h3><ul><li>\u53BB\u817E\u8BAF\u5C0F\u7A0B\u5E8F\u5B98\u7F51\u6CE8\u518C\u8D26\u53F7\uFF0C\u62FF\u5230appid\u548Csecret\u4FE1\u606F</li><li>\u4E0B\u8F7D\u597D\u5FAE\u4FE1\u5C0F\u7A0B\u5E8F\u5F00\u53D1\u5DE5\u5177</li><li>\u6253\u5F00\u5DE5\u5177\uFF0C\u5BFC\u5165\u4EE3\u7801 \\source\\wx\\xzs-student</li><li>\u4FEE\u6539application.yml\u6587\u4EF6\u91CC\u7684wx\u914D\u7F6E\u4E0B\u9762\u7684appid\u548Csecret</li><li>\u542F\u52A8\u5FAE\u4FE1\u5C0F\u7A0B\u5E8F\u5F00\u53D1\u5DE5\u5177</li></ul><h3 id="_5-6-\u4EE3\u7801\u7ED3\u6784\u7B80\u4ECB" tabindex="-1"><a class="header-anchor" href="#_5-6-\u4EE3\u7801\u7ED3\u6784\u7B80\u4ECB" aria-hidden="true">#</a> 5.6 \u4EE3\u7801\u7ED3\u6784\u7B80\u4ECB</h3><h4 id="_5-6-1-\u540E\u7AEF\u7CFB\u7EDF" tabindex="-1"><a class="header-anchor" href="#_5-6-1-\u540E\u7AEF\u7CFB\u7EDF" aria-hidden="true">#</a> 5.6.1 \u540E\u7AEF\u7CFB\u7EDF</h4><div class="language-text ext-text line-numbers-mode"><pre class="language-text"><code>- source \u4EE3\u7801\u6587\u4EF6\u5939
+ - xzs \u540E\u7AEF\u4EE3\u7801
+  - src 
+   - main
+    - java
+      - xzs
+       - base   \u9879\u76EE\u57FA\u7840\u7C7B
+       - configuration   springboot\u7684\u57FA\u7840\u914D\u7F6E
+       - context   \u9879\u76EE\u4E0A\u4E0B\u6587
+       - controller
+        - admin   \u7BA1\u7406\u7AEF\u63A7\u5236\u5668
+        - student   \u5B66\u751F\u7AEF\u63A7\u5236\u5668
+        - wechat   \u5C0F\u7A0B\u5E8F\u63A7\u5236\u5668
+       - domain   \u9886\u57DF\u5BF9\u8C61
+       - event   \u4E8B\u4EF6\u9A71\u52A8\u6A21\u578B\uFF0C\u914D\u5408\u76D1\u542C\u5668\u4E00\u8D77\u4F7F\u7528
+       - exception   \u4E1A\u52A1\u5F02\u5E38
+       - listener   \u76D1\u542C\u5668\uFF0C\u914D\u5408event\u4F7F\u7528
+       - repository   \u6570\u636E\u4ED3\u50A8\uFF0Cmybaties\u7684\u6620\u5C04Mapper
+       - service   \u670D\u52A1
+        - impl   \u670D\u52A1\u5B9E\u73B0
+       - utility   \u5DE5\u5177\u7C7B
+       - viewmodel   \u89C6\u56FE\u3001\u63A5\u53E3\u7C7B
+        - admin   \u7BA1\u7406\u7AEF\u89C6\u56FE\u7C7B
+        - student   \u5B66\u751F\u7AEF\u89C6\u56FE\u7C7B
+        - wx   \u5FAE\u4FE1\u7AEF\u89C6\u56FE\u7C7B
+      - resources
+       - mapper   \u6570\u636E\u4ED3\u5E93\u7684sql\u6587\u4EF6
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h4 id="_5-6-2-\u5B66\u751F\u7AEF" tabindex="-1"><a class="header-anchor" href="#_5-6-2-\u5B66\u751F\u7AEF" aria-hidden="true">#</a> 5.6.2 \u5B66\u751F\u7AEF</h4><div class="language-text ext-text line-numbers-mode"><pre class="language-text"><code>- source \u4EE3\u7801\u6587\u4EF6\u5939
+ - vue \u540E\u7AEF\u4EE3\u7801
+  - xzs-student  \u5B66\u751F\u7AEF\u4EE3\u7801
+   - public \u516C\u5171\u6587\u4EF6\u3001\u4E3B\u9875
+   - src \u524D\u7AEF\u4EE3\u7801
+    - api \u63A5\u53E3\u5730\u5740
+    - assets \u56FE\u7247\u8D44\u6E90
+    - components \u516C\u5171\u7EC4\u4EF6
+     - BackToTop \u8FD4\u56DE\u9876\u90E8\u7EC4\u4EF6
+     - Pagination \u5206\u9875\u7EC4\u4EF6
+     - SvgIcon \u56FE\u6807\u7EC4\u4EF6
+     - Ueditor \u7F16\u8F91\u5668\u7EC4\u4EF6
+    - icons \u56FE\u6807\u5E93
+    - layout \u6BCD\u7248\u9875
+    - store \u72B6\u6001\u7BA1\u7406
+    - styles \u4E3B\u9898\u6837\u5F0F
+    - utils \u516C\u5171\u65B9\u6CD5
+    - views \u89C6\u56FE\u3001\u7CFB\u7EDF\u9875\u9762
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h4 id="_5-6-3-\u5FAE\u4FE1\u7AEF" tabindex="-1"><a class="header-anchor" href="#_5-6-3-\u5FAE\u4FE1\u7AEF" aria-hidden="true">#</a> 5.6.3 \u5FAE\u4FE1\u7AEF</h4><div class="language-text ext-text line-numbers-mode"><pre class="language-text"><code>- source \u4EE3\u7801\u6587\u4EF6\u5939
+ - wx \u5FAE\u4FE1\u7AEF\u4EE3\u7801
+  - xzs-student  \u5B66\u751F\u7AEF\u4EE3\u7801
+   - assets \u56FE\u7247\u8D44\u6E90\u6587\u4EF6
+   - component \u7EC4\u4EF6\u5E93
+    - iView \u4E3B\u9898
+   - pages \u5C0F\u7A0B\u5E8F\u9875\u9762
+   - utils \u516C\u5171\u65B9\u6CD5
+   - wxs \u9875\u9762\u516C\u5171\u65B9\u6CD5
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h4 id="_5-6-4-\u7BA1\u7406\u7AEF" tabindex="-1"><a class="header-anchor" href="#_5-6-4-\u7BA1\u7406\u7AEF" aria-hidden="true">#</a> 5.6.4 \u7BA1\u7406\u7AEF</h4><div class="language-text ext-text line-numbers-mode"><pre class="language-text"><code>- source \u4EE3\u7801\u6587\u4EF6\u5939
+ - vue \u540E\u7AEF\u4EE3\u7801
+  - xzs-admin  \u7BA1\u7406\u7AEF\u4EE3\u7801
+   - public \u516C\u5171\u6587\u4EF6\u3001\u4E3B\u9875
+   - src \u524D\u7AEF\u4EE3\u7801
+    - api \u63A5\u53E3\u5730\u5740
+    - assets \u56FE\u7247\u8D44\u6E90
+    - components \u516C\u5171\u7EC4\u4EF6
+     - BackToTop \u8FD4\u56DE\u9876\u90E8\u7EC4\u4EF6
+     - Pagination \u5206\u9875\u7EC4\u4EF6
+     - SvgIcon \u56FE\u6807\u7EC4\u4EF6
+     - Ueditor \u7F16\u8F91\u5668\u7EC4\u4EF6
+    - icons \u56FE\u6807\u5E93
+    - layout \u6BCD\u7248\u9875
+    - store \u72B6\u6001\u7BA1\u7406
+    - styles \u4E3B\u9898\u6837\u5F0F
+    - utils \u516C\u5171\u65B9\u6CD5
+    - views \u89C6\u56FE\u3001\u7CFB\u7EDF\u9875\u9762
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,18);function M(P,U){const i=d("ExternalLinkIcon");return a(),r("div",null,[o,v,e("table",null,[u,e("tbody",null,[e("tr",null,[m,h,e("td",null,[e("a",b,[n("https://nodejs.org/download/release/latest-v16.x/"),s(i)])])]),e("tr",null,[_,p,e("td",null,[e("a",x,[n("https://www.oracle.com/java/technologies/downloads/#java8-windows"),s(i)])])]),e("tr",null,[g,w,e("td",null,[e("a",f,[n("https://dev.mysql.com/downloads/windows/installer/8.0.html"),s(i)])])])])]),y,e("ul",null,[e("li",null,[n("\u6570\u636E\u5E93\u811A\u672C\u4E0B\u8F7D\u5730\u5740\uFF1A"),e("a",k,[n("https://www.mindskip.net:999"),s(i)]),n("\uFF0C\u81EA\u52A9\u83B7\u53D6\u5230\u6570\u636E\u5E93\u811A\u672C \uFF0C\u521B\u5EFA\u8868\u521D\u59CB\u5316\u6570\u636E\uFF0C\u6570\u636E\u5E93\u540D\u79F0\u4E3Axzs")]),z,q,j,I]),B,e("ul",null,[N,T,V,E,e("li",null,[n("\u5B66\u751F\u7CFB\u7EDF\u5730\u5740\uFF1A"),e("a",J,[n("http://ip:8000/student"),s(i)])]),e("li",null,[n("\u7BA1\u7406\u7AEF\u5730\u5740\uFF1A"),e("a",S,[n("http://ip:8000/admin"),s(i)])])]),L])}const C=l(c,[["render",M],["__file","develop.html.vue"]]);export{C as default};
